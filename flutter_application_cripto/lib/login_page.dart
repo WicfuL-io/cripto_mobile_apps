72c6dca2a6e907948adcb113a0facaf9c54e593e
WicfuL <114489545+WicfuL-io@users.noreply.github.com>
2026-01-19 13:37:09 +0800

    Update login URL for backend connection

--- flutter_application_cripto/lib/login_page.dart
+++ flutter_application_cripto/lib/login_page.dart
@@ -22,7 +22,7 @@ class _LoginPageState extends State<LoginPage> {
   Future<void> loginUser() async {
     setState(() => isLoading = true);
 
-    final url = Uri.parse("http://localhost/projectcripto/login.php"); //sesuaikan dengan localhost db
+    final url = Uri.parse("http://localhost/cripto_db_backend/login.php"); //sesuaikan dengan localhost db
     final hashed = hashPassword(passwordController.text);
 
     try {
